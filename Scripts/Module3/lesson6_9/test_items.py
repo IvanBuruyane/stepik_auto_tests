aeed7ae4c007a3d07d4bfabf71862a80970ad64b
iburuyane <iburuyane@gmail.com>
2020-01-19 13:26:29 +0200

    Fixed css selector in add_to_basket_button

--- Scripts/Module3/lesson6_9/test_items.py
+++ Scripts/Module3/lesson6_9/test_items.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 def test_add_to_basket_button_exists(browser):
     link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    add_to_basket_button=browser.find_elements_by_css_selector("button.btn-add-to-basket1")
+    add_to_basket_button=browser.find_elements_by_css_selector("button.btn-add-to-basket")
     #print(len(add_to_basket_button))
     assert len(add_to_basket_button)==1 , "There is no ADD_TO_BASKET_BUTTON on the page"
 
